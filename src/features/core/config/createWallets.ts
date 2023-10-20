65da96f19f24cd5bd897caf191fb66e41cf624b8
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-10-20 18:47:08 -0400

    WIP support walletconnect - cannot send ETH from poly or arb

--- src/features/core/config/createWallets.ts
+++ src/features/core/config/createWallets.ts
@@ -12,6 +12,8 @@ type ArrayOneOrMore<T> = {
   0: T;
 } & Array<T>;
 
+const TELOS_MAINNET_CHAIN_ID = 40;
+
 export function createWallets(chains: ChainId[]): Record<string, Wallet<unknown>> {
   const wallets: Record<string, Wallet<unknown>> = {};
 
@@ -22,14 +24,17 @@ export function createWallets(chains: ChainId[]): Record<string, Wallet<unknown>
   wallets.braveWallet = new BraveWallet();
   wallets.phantomEvm = new PhantomWalletEvm();
 
-  // const evmChains = chains.map(toEvmChainId) as ArrayOneOrMore<number>;
+  const evmChains = chains.map(toEvmChainId) as ArrayOneOrMore<number>;
 
   // Get projectId from WalletConnect cloud https://docs.walletconnect.com/advanced/migration-from-v1.x/dapps#ethereum-provider
-  // wallets.walletConnect = new WalletConnect({
-  //   projectId: '',
-  //   showQrModal: true,
-  //   optionalChains: evmChains,
-  // });
+  wallets.walletConnect = new WalletConnect({
+    projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string, // eztodo add this to readme
+    showQrModal: true,
+    optionalChains: evmChains,
+    rpcMap: {
+      [TELOS_MAINNET_CHAIN_ID]: 'https://mainnet.telos.net:443/evm',
+    },
+  });
 
   if (typeof window !== 'undefined') {
     Object.values(wallets).forEach((wallet) => wallet.autoConnect());
