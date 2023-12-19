ac268fb20efe2ee0f3745a202bfaf380a9c4866b
donnyquixotic <don@caleos.io>
2023-12-19 12:04:21 -0600

    fix: update oft token assets

--- src/features/core/ui/CurrencyIcon.tsx
+++ src/features/core/ui/CurrencyIcon.tsx
@@ -13,15 +13,31 @@ type CurrencyIconProps = {
 
 const Image = styled('img')(() => ({}));
 
+const getTokenIcon = (symbol: string): string => {
+  switch(symbol) {
+    case 'BANANA':
+      return 'https://raw.githubusercontent.com/telosnetwork/token-list/master/logos/banana-logo.png';
+    case 'LVC':
+      return 'https://raw.githubusercontent.com/telosnetwork/token-list/master/logos/LVC.png';
+    case 'RF':
+      return 'https://raw.githubusercontent.com/telosnetwork/token-list/master/logos/RF.webp';
+    case 'VC':
+      return 'https://raw.githubusercontent.com/telosnetwork/token-list/master/logos/VC.png';
+    default:
+      return getCurrencyIcon(symbol);
+  }
+};
+
 export const CurrencyIcon: React.FC<CurrencyIconProps> = (props) => {
   const {size, currency, ...otherProps} = props;
   const defaultUrl = getCurrencyIcon('default');
   if (!currency) {
     return <Icon type='emptyToken' size={props.size} />;
   }
+
   return (
     <Image
-      src={getCurrencyIcon(fiatStore.getSymbol(currency))}
+      src={getTokenIcon(fiatStore.getSymbol(currency))}
       width={size}
       height={size}
       onError={overrideImageSrcOnError(defaultUrl)}
