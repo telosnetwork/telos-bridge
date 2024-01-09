165026cf2072ba6bfedcda05011fda81878a7c8b
donnyquixotic <don@caleos.io>
2024-01-09 00:39:29 -0600

    fix: set desired network list

--- src/features/bridge/stores/bridgeStore.tsx
+++ src/features/bridge/stores/bridgeStore.tsx
@@ -101,6 +101,12 @@ export class BridgeStore {
     );
   }
 
+  static sortChains(chains: ChainId[]): ChainId[] {
+    // desired order of network selection list
+    const chainOrder = [ChainId.TELOS, ChainId.ETHEREUM, ChainId.POLYGON, ChainId.BSC, ChainId.ZKSYNC, ChainId.ZKCONSENSYS, ChainId.AVALANCHE,  ChainId.ARBITRUM];
+    return chains.sort((a,b) => chainOrder.indexOf(b) - chainOrder.indexOf(a));
+  }
+
   // views
   get output(): BridgeOutput | undefined {
     return fromPromiseValue(this.promise.output);
@@ -144,7 +150,8 @@ export class BridgeStore {
   }
 
   get chains(): ChainId[] {
-    return Array.from(new Set(this.currencies.map((c) => c.chainId)));
+    const chainList = Array.from(new Set(this.currencies.map((c) => c.chainId)));
+    return BridgeStore.sortChains(chainList);
   }
 
   get srcCurrencyOptions(): CurrencyOption[] {
@@ -219,7 +226,9 @@ export class BridgeStore {
   }
 
   get srcNetworkOptions(): ChainOption[] {
-    const chains = Array.from(new Set(this.srcCurrencyOptions.map((c) => c.currency.chainId)));
+    const chainList = Array.from(new Set(this.srcCurrencyOptions.map((c) => c.currency.chainId)));
+    const chains = BridgeStore.sortChains(chainList);
+
     return chains.map((chainId) => ({
       chainId,
       disabled: false,
