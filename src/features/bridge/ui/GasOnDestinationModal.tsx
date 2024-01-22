902c4234aa0722e571ae7eb5664482140b703fdf
donnyquixotic <don@caleos.io>
2024-01-22 10:50:49 -0600

    feat: add fathom analytics

--- src/features/bridge/ui/GasOnDestinationModal.tsx
+++ src/features/bridge/ui/GasOnDestinationModal.tsx
@@ -50,10 +50,9 @@ export const GasOnDestinationModal = observer((props: GasOnDestinationModalProps
           <Box component='span' color='primary.main'>
             {dstNative?.symbol}
           </Box>{' '}
-          you want to swap.
+          you want to receive at the destination.
           <br />
-          The total amount you’ll pay in your wallet includes the gas you’ll be airdropping to your
-          destination.
+          Choose the amount of TLOS you want to recieve at the destination. The total amount you`&apos;`ll pay in your wallet includes the gas you`&apos;`ll be airdropping.
         </Box>
         <Box sx={{mt: 4, mb: 2}}>
           <Selector selection={form.dstNativeAmount}>
