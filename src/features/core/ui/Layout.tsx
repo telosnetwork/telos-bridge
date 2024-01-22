ba621c7df098227496ee832ea4b41f30edde4ccf
donnyquixotic <don@caleos.io>
2024-01-22 11:06:24 -0600

    fix: header links on mobile

--- src/features/core/ui/Layout.tsx
+++ src/features/core/ui/Layout.tsx
@@ -60,10 +60,10 @@ export const AppHeader = observer(() => {
             <Logo src={'/static/telos-logo.png'} width={95} height={112} alt='Telos Blockchain Logo' priority={true} />
           </NavLink>
         </Link>
-        <NavLink sx={{ margin:{xs: "auto", md: 'unset'}}} href='https://stakely.io/en/faucet/telos-evm-tlos' target='_blank' rel="noreferrer">
+        <NavLink sx={{ 'margin-left':{xs: "auto", md: 'unset'}}} href='https://stakely.io/en/faucet/telos-evm-tlos' target='_blank' rel="noreferrer">
           Faucet 
         </NavLink>
-        <NavLink href='https://dapp.ptokens.io/#/swap?asset=tlos&from=eth&to=telos' target='_blank' rel="noreferrer" >
+        <NavLink sx={{ 'margin-right':{xs: "auto", md: 'unset'}}} href='https://dapp.ptokens.io/#/swap?asset=tlos&from=eth&to=telos' target='_blank' rel="noreferrer" >
           Bridge TLOS (pToken)
         </NavLink>
         {/* <Link href='/oft' passHref legacyBehavior>
