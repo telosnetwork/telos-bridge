e9006f280a99b5e374b0015f38ba92bab7dd8643
rozzaswap <rozza@swapsicle.io>
2024-05-20 12:38:01 +0100

    add null check for navigator

--- src/features/core/utils/platform.ts
+++ src/features/core/utils/platform.ts
@@ -1,4 +1,7 @@
 export function isMobile() {
-    const mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i;
-    return mobileRegex.test(navigator.userAgent);
+    if (typeof navigator !== 'undefined') {
+        const mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i;
+        return mobileRegex.test(navigator.userAgent);
+    }
+    return false;
 }
